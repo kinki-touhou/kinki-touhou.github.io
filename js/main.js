02b73ea24bee802fdf661408a8e84fd3b850cf55
HOOBIG <th27tanahoda@gmail.com>
2025-12-14 23:05:14 +0900

    [update]script→jsファイルに移行
    一部scriptを.jsに変更

--- js/main.js
+++ js/main.js
@@ -26,8 +26,10 @@ function createPetal() {
 
 // 300ミリ秒（0.3秒）ごとに花びらを1枚生成する
 setInterval(createPetal, 300);
+/*桜ここまで*/
 
-window.addEventListener('load', function() {
+/*ローディング画面*/
+function loadScreen() {
             // 画面の読み込みが完全に終わったら実行
             const loadingScreen = document.getElementById('loading');
             
@@ -35,4 +37,31 @@ window.addEventListener('load', function() {
             setTimeout(function() {
                 loadingScreen.classList.add('loaded');
             }, 800);
-        });
+
+            
+        };
+/*ローディング画面ここまで*/
+
+/*カウントダウン*/
+function countDownKouroumu(){
+        const eventDate = new Date("October 11, 2026 10:00:00").getTime();
+        const now = new Date().getTime();
+        const gap = eventDate - now;
+
+        // 計算（ミリ秒を日・時に直す）
+        const second = 1000;
+        const minute = second * 60;
+        const hour = minute * 60;
+        const day = hour * 24;
+
+        const textDay = Math.floor(gap / day);
+        const textHour = Math.floor((gap % day) / hour);
+
+        // 画面に表示
+        if (gap > 0) {
+            document.getElementById("days").innerText = textDay;
+            document.getElementById("hours").innerText = textHour;
+        } else {
+            document.getElementById("timer").innerText = "開催当日！";
+        }
+}
